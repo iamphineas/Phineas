32f7a10f1a94975ae2a68e3117816975cb2e0b7d
ST10295602 <ST10295602@vcconnect.edu.za>
2023-04-26 10:27:07 +0200

    First commit

---
+++ src/main/java/com/mycompany/lastexampleofaforloop/Lastexampleofaforloop.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lastexampleofaforloop;
+
+/**
+ *
+ * @author PAUL
+ */
+public class Lastexampleofaforloop {
+
+    public static void main(String[] args) {
+        int max = 9; 
+                for( int i=0; i<=max;i++) {
+                    for(int j=0;j<=max;j++) {
+                        System.out.printf("[%d,%d]",i,j);  
+                    
+          }
+                    System.out.println();       
+       }
+    }
+}
